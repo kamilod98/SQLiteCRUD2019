1bca2a0340597452473544839ad66641c67432cd
lobo98 <kamilo4023@gmail.com>
2019-10-18 09:43:22 -0500

    Validacion de mostrar contraseña

---
+++ Taller2/app/src/main/java/com/example/taller2/clases/UserListAdapter.java
@@ -0,0 +1,10 @@
+package com.example.taller2.clases;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class UserListAdapter extends ArrayAdapter<String> {
+
+    private final Context context;
+    private String[][] listItem;
+}
